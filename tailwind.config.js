f3a98cd7fe674707d287058dcb7982a5084fc710
Ruby Shi <ruby@mi-go.com.tw>
2019-07-11 11:39:16 +0800

    修改tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,17 +26,6 @@ module.exports = {
         800: '#909399',
         900: '#606266'
       },
-      // ,gray: {
-      //   100: '#f7fafc',
-      //   200: '#edf2f7',
-      //   300: '#e2e8f0',
-      //   400: '#cbd5e0',
-      //   500: '#a0aec0',
-      //   600: '#718096',
-      //   700: '#4a5568',
-      //   800: '#2d3748',
-      //   900: '#1a202c'
-      // },
 
       primary: {
         default: 'var(--primary)',
@@ -369,7 +358,7 @@ module.exports = {
     borderStyle: ['responsive'],
     borderWidth: ['responsive'],
     boxShadow: ['responsive', 'hover', 'focus', 'disabled'],
-    cursor: ['responsive'],
+    cursor: ['responsive', 'disabled'],
     display: ['responsive'],
     fill: ['responsive'],
     flex: ['responsive'],
