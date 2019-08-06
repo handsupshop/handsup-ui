e60db447d09f3b342feeefc0e54f3c9c923d274e
winnie <winnie@handsup.shop>
2019-08-06 17:07:51 +0800

    button 新增 loading 的樣式

--- src/components/BaseButton/index.vue
+++ src/components/BaseButton/index.vue
@@ -20,6 +20,14 @@ export default {
     outline: {
       type: [Boolean, String],
       default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
   },
   name: 'baseButton',
@@ -37,21 +45,16 @@ export default {
             return [
               `text-${this.color}-dark`,
               `bg-${this.color}-vr-light`,
-              `border-${this.color}-light`,
-              'hover:text-white',
-              `hover:bg-${this.color}`,
-              `hover:border-${this.color}`
+              `border-${this.color}-light`
             ]
           } else {
             return [
               'text-gray-900',
               'bg-gray-400',
-              'border-gray-600',
-              `hover:text-${this.color}`,
-              'hover:bg-primary-vr-light',
-              'hover:border-primary-light'
+              'border-gray-600'
             ]
           }
+          break
 
         // 純框線
         case true:
@@ -59,21 +62,16 @@ export default {
             return [
               'text-black',
               'bg-white',
-              'border-gray-600',
-              // 'hover:text-white',
-              // 'hover:bg-primary',
-              'hover:border-primary'
+              'border-gray-600'
             ]
           } else {
             return [
               'text-black',
               'bg-white',
-              `border-${this.color}`,
-              'hover:text-white',
-              `hover:bg-${this.color}`,
-              `hover:border-${this.color}`
+              `border-${this.color}`
             ]
           }
+          break
 
         // 純底色
         default:
@@ -81,21 +79,16 @@ export default {
             return [
               'text-white',
               `bg-${this.color}`,
-              `border-${this.color}`,
-              'hover:text-white',
-              `hover:bg-${this.color}-dark`,
-              `hover:border-${this.color}-dark`
+              `border-${this.color}`
             ]
           } else {
             return [
               'text-gray-900',
               'bg-gray-400',
-              'border-gray-400',
-              'hover:text-white',
-              'hover:bg-primary',
-              'hover:border-primary'
+              'border-gray-400'
             ]
           }
+          break
       }
     },
     exportSize () {
@@ -106,9 +99,68 @@ export default {
     exportDisplay () {
       if (this.block) return ['block', 'w-full']
       return ['inline-block', 'my-1', 'mr-1']
+    },
+    exportHover () {
+      let arry = []
+      if (this.disabled || this.loading) {
+        // disabled 樣式
+        arry.push('disabled:opacity-50', 'disabled:cursor-not-allowed')
+      }else{
+        // 非 disabled 的才要加上 hover 樣式
+        switch (this.outline) {
+          // 有線＋底色
+          case 'flat':
+            if (this.color !== 'basic') {
+              arry.push(
+                'hover:text-white',
+                `hover:bg-${this.color}`,
+                `hover:border-${this.color}`
+              )
+            } else {
+              arry.push(
+                'hover:text-gray-900',
+                'hover:bg-primary-vr-light',
+                'hover:border-primary-light'
+              )
+            }
+          break
+
+          // 純框線
+          case true:
+            if (this.color === 'basic') {
+              arry.push('hover:border-primary')
+            } else {
+              arry.push(
+                'hover:text-white',
+                `hover:bg-${this.color}`,
+                `hover:border-${this.color}`
+              )
+            }
+          break
+
+          // 純底色
+          default:
+            if (this.color !== 'basic') {
+              arry.push(
+                'hover:text-white',
+                `hover:bg-${this.color}-dark`,
+                `hover:border-${this.color}-dark`
+              )
+            } else {
+              arry.push(
+                'hover:text-white',
+                'hover:bg-primary',
+                'hover:border-primary'
+              )
+            }
+          break
+        }        
+      }
+      return arry
     }
   }
 }
 </script>
 
 <template lang="pug" src="./template.pug"></template>
+<style lang="scss" src="./style.scss" scoped></style>
