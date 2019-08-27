1bf423f0d71c207235878a9b35dd1affbb817a64
gigi <gigi@handsup.shop>
2019-08-27 11:46:39 +0800

    delete other page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import UiGuideline from './views/UiGuideline/index.vue'
 
 Vue.use(Router)
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'UiGuideline',
       component: UiGuideline
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: Home
     }
     // {
     //   path: '/about',
