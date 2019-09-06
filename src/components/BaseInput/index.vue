b5d7571714f3d6b0945adc149b2b1b2a5282b0df
gigi <gigi@handsup.shop>
2019-09-06 19:15:30 +0800

    baseinput

--- src/components/BaseInput/index.vue
+++ src/components/BaseInput/index.vue
@@ -1,15 +1,27 @@
 <script>
+const basicClass = []
+
+const error = ['text-black', 'border-danger', 'hover:border-danger', 'focus:border-danger']
+const sizeSm = ['py-2', 'text-xs', 'h-8']
+const sizeMd = ['py-2', 'text-sm', 'h-9']
+const sizeLg = ['p-3', 'text-base', 'h-10']
+
+const displayInline = ['inline-block', 'my-1', 'mr-1']
+const displayBlock = ['block', 'w-full']
+
 export default {
-  inheritAttrs: false,
   name: 'BaseInput',
+  inheritAttrs: false,
   props: {
+    value: [String, Number],
+    disabled: Boolean,
     color: {
       type: String,
       default: 'basic'
     },
     size: {
       type: String,
-      default: 'md'
+      default: ''
     },
     inline: {
       type: Boolean,
@@ -30,43 +42,44 @@ export default {
     iconPosition: {
       type: String,
       default: 'left'
+    },
+    showWordLimit: {
+      type: Boolean,
+      default: false
     }
   },
 
-  data () {
-    return {
+  mounted () {
+    if (this.showWordLimit) {
+      this.calcCountPadding()
     }
   },
+
+  updated () {
+    if (this.showWordLimit) {
+      this.$nextTick(this.calcCountPadding)
+    }
+  },
+
   computed: {
-    exportClass () {
-      if (this.color === 'basic') {
-        return [
-          'border-gray-600',
-          'hover:border-gray-700',
-          'focus:border-primary',
-          'disabled:bg-gray-100',
-          'disabled:border-gray-600',
-          'disabled:text-gray-700']
-      } else {
-        return [
-          // `text-${this.color}`,
-          `border-${this.color}`,
+    exportInputClass () {
+      return [
+        ...(this.color === 'basic' ? basicClass : [`border-${this.color}`,
           'disabled:bg-gray-200',
-          'disabled:opacity-50']
-      }
+          'disabled:opacity-50']),
+        ...(this.inline ? displayInline : displayBlock),
+        ...this.computedSize,
+        ...(this.isError ? error : '')
+      ]
     },
-    exportSize () {
-      if (this.size === 'sm') return ['py-1', 'text-xs']
-      if (this.size === 'lg') return ['p-3', 'text-base']
-      return ['py-2', 'text-sm']
+    computedSize () {
+      if (this.size === 'sm') return sizeSm
+      if (this.size === 'lg') return sizeLg
+      return sizeMd
     },
-    exportDisplay () {
+    exportClass () {
       if (this.inline) return ['inline-block', 'my-1', 'mr-1']
       return ['block', 'w-full']
-    },
-    exportError () {
-      if (this.isError) return 'text-black border-danger hover:border-danger focus:border-danger'
-      return ''
     },
     exportIcon () {
       if (this.iconPosition === 'right') return 'right-0'
@@ -79,11 +92,40 @@ export default {
         if (this.iconPosition === 'right') return 'pl-3 pr-10'
         return 'pl-10 pr-3'
       }
+    },
+    upperLimit () {
+      console.log(this.$attrs)
+
+      return this.$attrs.maxlength
+    },
+    textLength () {
+      if (typeof this.value === 'number') {
+        return String(this.value).length
+      }
+
+      return (this.value || '').length
+    },
+    inputExceed () {
+      // show exceed style if length of initial value greater then maxlength
+      return this.showWordLimit &&
+          (this.textLength > this.upperLimit)
+    },
+    inputDisabled () {
+      return this.disabled
     }
   },
   methods: {
-    handleInput () {
+    handleInput (event) {
       this.$emit('input', event.target.value)
+      console.log(event.target.value)
+      console.log('input value', this.$refs.input.value)
+      console.log('props value', this.value)
+    },
+    handleChange (event) {
+      this.$emit('change', event.target.value)
+    },
+    calcCountPadding () {
+      this.$refs.input.style.paddingRight = (this.$refs.inputCount.clientWidth + 8) + 'px'
     }
   }
 }
---
+++ src/views/BaseInput.vue
@@ -0,0 +1,100 @@
+<template lang="pug">
+	.container.m-auto
+
+		:markdown-it
+			# BaseInput
+			## 基礎用法
+
+		BaseInput(v-model="value1" placeholder="請輸入內容")
+
+		highlight-code(lang="javascript").
+			BaseInput(v-model="value1" placeholder="請輸入內容")
+
+			data() {
+				return {
+					value1: ''
+				}
+			}
+
+		:markdown-it
+			## 🈲️️用狀態
+
+			> 通过 disabled 属性指定是否禁用 input 组件
+
+		BaseInput(v-model="value2" placeholder="請輸入內容" disabled)
+
+		highlight-code(lang="javascript").
+			BaseInput(v-model="value2" placeholder="請輸入內容" disabled)
+
+		:markdown-it
+			## size
+			> 有 `sm` `md` `lg` 三種尺寸，預設是 `md`。
+		.flex.items-center
+			BaseInput.mr-2(v-model="value3" placeholder="請輸入內容" size="sm")
+			BaseInput.mr-2(v-model="value4" placeholder="請輸入內容" size="md")
+			BaseInput(v-model="value5" placeholder="請輸入內容" size="lg")
+		highlight-code(lang="javascript").
+			BaseInput(v-model="value3" placeholder="請輸入內容" size="sm")
+			BaseInput(v-model="value4" placeholder="請輸入內容" size="md")
+			BaseInput(v-model="value5" placeholder="請輸入內容" size="lg")
+
+		:markdown-it
+			## 狀態
+			> success error
+
+		BaseInput(v-model="value6" placeholder="請輸入內容" isError)
+		BaseInput(v-model="value7" placeholder="請輸入內容" isError error-message="必填")
+		highlight-code(lang="javascript").
+			BaseInput(v-model="value6" placeholder="請輸入內容" isError)
+			BaseInput(v-model="value7" placeholder="請輸入內容" isError error-message="必填")
+
+		:markdown-it
+			## 帶 Icon
+			> success error
+
+		BaseInput(v-model="value8" placeholder="請輸入內容" iconClass="fal fa-calendar-alt")
+		BaseInput(v-model="value9" placeholder="請輸入內容" iconClass="fal fa-search" iconPosition="right")
+		highlight-code(lang="javascript").
+			BaseInput(v-model="value8" placeholder="請輸入內容" iconClass="fal fa-calendar-alt")
+			BaseInput(v-model="value9" placeholder="請輸入內容" iconClass="fal fa-search" iconPosition="right")
+
+		:markdown-it
+			## 長度限制
+			> success error
+
+		BaseInput(v-model="value10" placeholder="請輸入內容" maxlength="30" show-word-limit)
+		highlight-code(lang="javascript").
+			BaseInput(v-model="value10" placeholder="請輸入內容" maxlength="30" show-word-limit)
+
+		:markdown-it
+			## Input Attributes
+
+</template>
+
+<script>
+import BaseInput from '@/components/BaseInput'
+
+export default {
+  components: {
+    BaseInput
+  },
+  data () {
+    return {
+      value1: '',
+      value2: '',
+      value3: '',
+      value4: '',
+      value5: '',
+      value6: '',
+      value7: '',
+      value8: '',
+      value9: '',
+      value10: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
